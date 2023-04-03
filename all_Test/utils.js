bef92554f87eb608a51c6bffcef4a25d340f7caf
astanelis <stanelisa@gmail.com>
2023-04-03 16:04:08 +0300

    pakeitimai

--- all_Test/utils.js
+++ all_Test/utils.js
@@ -1,33 +1,31 @@
 function getUser(username) {
-    const users = JSON.parse(localStorage.getItem('users')) || {};
-    return users[username] || null;
-  }
-  
-  function saveUser(user) {
-    const users = JSON.parse(localStorage.getItem('users')) || {}
-    user.tasks = user.tasks || []
-    users[user.username] = user
-    localStorage.setItem('users', JSON.stringify(users))
-  }
-  
-  function showAlert(message, className, formType) {
-    const alertContainer = document.getElementById(`${formType}-alert`);
-  
-    // Update the alert container content
-    alertContainer.textContent = message;
-    alertContainer.classList.add(className);
-    alertContainer.style.display = 'block';
-  
-    // Remove the alert message after 3 seconds
-    setTimeout(() => {
-      alertContainer.textContent = '';
-      alertContainer.classList.remove(className);
-      alertContainer.style.display = 'none';
-    }, 3000);
-  }
-  
-  module.exports = {
-    getUser,
-    saveUser,
-    showAlert,
-  };
+  const users = JSON.parse(localStorage.getItem('users')) || {}
+  return users[username] || null
+}
+
+function saveUser(user) {
+  const users = JSON.parse(localStorage.getItem('users')) || {}
+  user.tasks = user.tasks || []
+  users[user.username] = user
+  localStorage.setItem('users', JSON.stringify(users))
+}
+
+function showAlert(message, className, formType) {
+  const alertContainer = document.getElementById(`${formType}-alert`)
+
+  alertContainer.textContent = message
+  alertContainer.classList.add(className)
+  alertContainer.style.display = 'block'
+
+  setTimeout(() => {
+    alertContainer.textContent = ''
+    alertContainer.classList.remove(className)
+    alertContainer.style.display = 'none'
+  }, 3000)
+}
+
+module.exports = {
+  getUser,
+  saveUser,
+  showAlert,
+}
--- all_Test/utils.test.js
+++ all_Test/utils.test.js
@@ -1,32 +1,26 @@
-const { getUser, saveUser, showAlert } = require('./utils');
+const { getUser, saveUser, showAlert } = require('./utils')
 
 describe('getUser', () => {
   beforeEach(() => {
-    // Clear local storage before each test
-    localStorage.clear();
-  });
+    localStorage.clear()
+  })
 
   it('should return null if the user does not exist', () => {
-    const user = getUser('nonexistent');
-    expect(user).toBeNull();
-  });
+    const user = getUser('nonexistent')
+    expect(user).toBeNull()
+  })
 
   it('should return the user object if the user exists', () => {
     const testUser = {
       username: 'testuser',
       password: 'password123',
       tasks: [],
-    };
+    }
 
-    // Save the test user to local storage
-    saveUser(testUser);
+    saveUser(testUser)
 
-    // Retrieve the user from local storage
-    const user = getUser('testuser');
+    const user = getUser('testuser')
 
-    // Test if the retrieved user matches the test user object
-    expect(user).toEqual(testUser);
-  });
-});
-
-// Write tests for saveUser and showAlert functions
+    expect(user).toEqual(testUser)
+  })
+})
